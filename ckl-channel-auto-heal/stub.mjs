0ede2524f188f78ec348a61124bb9a27fe8cb944
dmrickey <dmrickey@gmail.com>
2022-07-08 18:09:47 -0500

    Automatically heal targets for heal abilities

--- ckl-channel-auto-heal/stub.mjs
+++ ckl-channel-auto-heal/stub.mjs
@@ -5,9 +5,19 @@ Hooks.once("socketlib.ready", () => {
     socket.register("updateActors", updateActors);
 });
 
-async function updateActors(updates) {
+async function updateActors(updates, amount) {
     if (updates?.length) {
         await Actor.updateDocuments(updates);
+
+        const ids = updates.map(x => x._id);
+        const actors = game.actors.filter(x => ids.includes(x.id));
+
+        let chatOptions = {
+            user: game.user._id,
+            speaker: ChatMessage.getSpeaker(),
+            content: `Automatically healed for [[${amount}[positive]]]: ${actors.map(x => x.name).join(', ')}`
+        };
+        ChatMessage.create(chatOptions);
     }
 }
 
@@ -16,7 +26,7 @@ const self = (me) => typeof me === 'function' ? me() : me;
 const ifDebug = (func) => {
     // todo read game setting
     // eslint-disable-next-line
-    if (true) {
+    if (false) {
         return self(func);
     }
 };
@@ -24,33 +34,36 @@ const ifDebug = (func) => {
 Hooks.once('ready', () => {
     Hooks.on('itemUse', async (item, type, etc) => {
         if (type === 'attack') {
-            // ifDebug(() => console.log('************** itemUse hook ********************', item, type, etc));
+            ifDebug(() => console.log('************** itemUse hook ********************', item, type, etc));
         }
         if (type === 'postAttack') {
-            // ifDebug(() => console.log('******************* postAttack hook *********************', item, type, etc));
-            if (item.name.toLowerCase().includes('channel energy') && etc.ev?.caught !== true) {
-                if (item.isHealing) {
-                    const healed = etc.templateData.attacks[0]?.damage.total;
-                    if (!healed) {
-                        return;
-                    }
-
-                    const targets = etc.templateData.targets;
-                    const updates = targets?.map((target) => {
-                        const currentHp = target.actorData.data.attributes.hp.value;
-                        const maxHp = target.actorData.data.attributes.hp.max;
-                        const newHp = Math.min(currentHp + healed, maxHp);
-                        return { _id: target.actorData._id, 'data.attributes.hp.value': newHp };
-                    })
-                    if (updates?.length) {
-                        await socket.executeAsGM('updateActors', updates);
-                    }
+            ifDebug(() => console.log('******************* postAttack hook *********************', item, type, etc));
+            // if (item.name.toLowerCase().includes('channel energy') && etc.ev?.caught !== true) {
+            if (item.isHealing) {
+                const healed = etc.templateData.attacks[0]?.damage.total;
+                if (!healed) {
+                    return;
                 }
 
-                if (item.hasTemplate) {
-                    canvas.scene.templates.get(etc.chatData["flags.pf1.metadata"].template).delete();
+                const myDisposition = item.actor.getActiveTokens()?.[0]?.data.disposition ?? 0;
+
+                let { targets } = etc.templateData;
+                targets = targets.filter(t => !myDisposition || targets.length === 1 || myDisposition === t.tokenData.disposition);
+                const updates = targets?.map((target) => {
+                    const currentHp = target.actorData.data.attributes.hp.value;
+                    const maxHp = target.actorData.data.attributes.hp.max;
+                    const newHp = Math.min(currentHp + healed, maxHp);
+                    return { _id: target.actorData._id, 'data.attributes.hp.value': newHp };
+                })
+                if (updates?.length) {
+                    await socket.executeAsGM('updateActors', updates, healed);
                 }
             }
+
+            if (item.hasTemplate) {
+                // canvas.scene.templates.get(etc.chatData["flags.pf1.metadata"].template).delete();
+            }
+            // }
         }
     });
 });
