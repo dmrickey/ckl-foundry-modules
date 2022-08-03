8c19de37340348a10393c89d9ac0569e55730cae
dmrickey <dmrickey@gmail.com>
2022-08-03 14:47:28 -0500

    Made heal message translateable

--- ckl-heal-friendly-targets/main.mjs
+++ ckl-heal-friendly-targets/main.mjs
@@ -1,21 +1,28 @@
 let socket;
 
+const MODULE_NAME = 'ckl-heal-friendly-targets';
+
 Hooks.once("socketlib.ready", () => {
-    socket = socketlib.registerModule("ckl-heal-friendly-targets");
+    socket = socketlib.registerModule(MODULE_NAME);
     socket.register("updateActors", updateActors);
 });
 
-async function updateActors(updates, amount) {
+async function updateActors(data, updates, amountHealed) {
     if (updates?.length) {
         await Actor.updateDocuments(updates);
 
         const ids = updates.map(x => x._id);
         const actors = game.actors.filter(x => ids.includes(x.id));
+        const targets = actors.map(x => x.name).join(', ');
+        const content = game.i18n.format(
+            `${MODULE_NAME}.healCardDescription`,
+            { amountHealed, targets },
+        );
 
         let chatOptions = {
             user: game.user._id,
             speaker: ChatMessage.getSpeaker(),
-            content: `Automatically healed for [[${amount}[positive]]]: ${actors.map(x => x.name).join(', ')}`
+            content,
         };
         ChatMessage.create(chatOptions);
     }
@@ -47,14 +54,16 @@ Hooks.once('ready', () => {
 
                 let { targets } = etc.templateData;
                 targets = targets.filter(t => !myDisposition || targets.length === 1 || myDisposition === t.tokenData.disposition);
-                const updates = targets?.map((target) => {
+                const data = targets?.map((target) => {
                     const currentHp = target.actorData.data.attributes.hp.value;
                     const maxHp = target.actorData.data.attributes.hp.max;
                     const newHp = Math.min(currentHp + healed, maxHp);
-                    return { _id: target.actorData._id, 'data.attributes.hp.value': newHp };
-                })
+                    return { id: target.actorData._id, currentHp, newHp };
+                }) ?? [];
+                const updates = data.map(({ id, newHp }) => ({ _id: id, newHp }));
+                // });
                 if (updates?.length) {
-                    await socket.executeAsGM('updateActors', updates, healed);
+                    await socket.executeAsGM('updateActors', data, updates, healed);
                 }
             }
         }
