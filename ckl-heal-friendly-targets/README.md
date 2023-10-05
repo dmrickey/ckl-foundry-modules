b55d29cc4cd44b69f2b9463cb0104d105d328e7e
dmrickey <dmrickey@gmail.com>
2023-10-05 13:30:09 -0500

    Updated heal friendly targets

--- ckl-heal-friendly-targets/README.md
+++ ckl-heal-friendly-targets/README.md
@@ -1,26 +1,11 @@
-# Stub Module
+# Auto Heal Friendly Targets
 
-I am a stub module. I do nothing but show the most basic structure of a functional module.
+If a healing ability is used, this automatically heals all targets that match the disposition of the actor that used the ability. If you specfifically want to heal a target with a different disposition, then you'll need to do that the old fashioned way. A chat card is printed out for each actor healed for verification purposes -- and if someone is healed that isn't then you know what needs undone.
 
-# Basic structure
+## Manifest
 
-| File | Explanation |
-| :--- | :--- |
-| [module.json](./module.json) | Mandatory [JSON](https://en.wikipedia.org/wiki/JSON) file that describes basic info about the module. |
-| [LICENCE](./LICENSE) | Optional. File that describes the license your module is distributed under. Highly recommended you include [some form of license](https://choosealicense.com/), even if it's meant for public domain. |
-| [README.md](./README.md) | Markdown readme file for explaining your module's function. |
-| [stub.mjs](./stub.mjs) | Custom & optional. ES6 module that holds the primary starting point of your module's script as described in the example `module.json`. |
-| [stub.css](./stub.css) | Custom & optional. CSS file as described in example `module.json` |
-| [lang.en.json](./lang.en.json) | Custom & optional. JSON file descibing language strings for use with game.i18n.localize() function for easier translation support. |
-
-## Additional resources
-
-For more indepth reading:
-- https://foundryvtt.com/article/module-development/ – Official documentation
-- https://foundryvtt.wiki/en/basics/Modules – Community Wiki
+https://github.com/dmrickey/ckl-foundry-modules/blob/main/ckl-heal-friendly-targets/module.json
 
 ## License
 
-It's nice to mention the license name clearly without making everyone look through the _LICENSE_ file.
-
-Remember you're also working under the [FVTT's Module Development License](https://foundryvtt.com/article/license/). This does not need to be mentioned in your own module, however.
+MIT
--- ckl-heal-friendly-targets/main.mjs
+++ ckl-heal-friendly-targets/main.mjs
@@ -2,31 +2,34 @@ let socket;
 
 const MODULE_NAME = 'ckl-heal-friendly-targets';
 
-Hooks.once("socketlib.ready", () => {
-    socket = socketlib.registerModule(MODULE_NAME);
-    socket.register("updateActors", updateActors);
-});
+const getFirstActiveGM = () => game.users.activeGM;
+const isFirstGM = () => getFirstActiveGM() === game.user;
 
-async function updateActors(data, updates, amountHealed) {
-    if (updates?.length) {
-        await Actor.updateDocuments(updates);
+// Hooks.once("socketlib.ready", () => {
+//     socket = socketlib.registerModule(MODULE_NAME);
+//     socket.register("updateActors", updateActors);
+// });
 
-        const ids = updates.map(x => x._id);
-        const actors = game.actors.filter(x => ids.includes(x.id));
-        const targets = actors.map(x => x.name).join(', ');
-        const content = game.i18n.format(
-            `${MODULE_NAME}.healCardDescription`,
-            { amountHealed, targets },
-        );
+// async function updateActors(data, updates, amountHealed) {
+//     if (updates?.length) {
+//         await Actor.updateDocuments(updates);
 
-        let chatOptions = {
-            user: game.user._id,
-            speaker: ChatMessage.getSpeaker(),
-            content,
-        };
-        ChatMessage.create(chatOptions);
-    }
-}
+//         const ids = updates.map(x => x._id);
+//         const actors = game.actors.filter(x => ids.includes(x.id));
+//         const targets = actors.map(x => x.name).join(', ');
+//         const content = game.i18n.format(
+//             `${MODULE_NAME}.healCardDescription`,
+//             { amountHealed, targets },
+//         );
+
+//         let chatOptions = {
+//             user: game.user._id,
+//             speaker: ChatMessage.getSpeaker(),
+//             content,
+//         };
+//         ChatMessage.create(chatOptions);
+//     }
+// }
 
 const self = (me) => typeof me === 'function' ? me() : me;
 
@@ -37,35 +40,82 @@ const ifDebug = (func) => {
     }
 };
 
-Hooks.once('ready', () => {
-    Hooks.on('itemUse', async (item, type, etc) => {
-        if (type === 'attack') {
-            ifDebug(() => console.log('************** itemUse hook ********************', item, type, etc));
-        }
-        if (type === 'postAttack') {
-            ifDebug(() => console.log('******************* postAttack hook *********************', item, type, etc));
-            if (item.isHealing) {
-                const healed = etc.templateData.attacks[0]?.damage.total;
-                if (!healed) {
-                    return;
-                }
+// Hooks.on('itemUse', async (item, type, etc) => {
+//     if (type === 'attack') {
+//         ifDebug(() => console.log('************** itemUse hook ********************', item, type, etc));
+//     }
+//     if (type === 'postAttack') {
+//         ifDebug(() => console.log('******************* postAttack hook *********************', item, type, etc));
+//         if (item.isHealing) {
+//             const healed = etc.templateData.attacks[0]?.damage.total;
+//             if (!healed) {
+//                 return;
+//             }
 
-                const myDisposition = item.actor.getActiveTokens()?.[0]?.data.disposition ?? 0;
+//             const myDisposition = item.actor?.getActiveTokens()?.[0]?.data.disposition ?? 0;
 
-                let { targets } = etc.templateData;
-                targets = targets.filter(t => !myDisposition || targets.length === 1 || myDisposition === t.tokenData.disposition);
-                const data = targets?.map((target) => {
-                    const currentHp = target.actorData.data.attributes.hp.value;
-                    const maxHp = target.actorData.data.attributes.hp.max;
-                    const newHp = Math.min(currentHp + healed, maxHp);
-                    return { id: target.actorData._id, currentHp, newHp };
-                }) ?? [];
-                const updates = data.map(({ id, newHp }) => ({ _id: id, 'data.attributes.hp.value': newHp }));
-                // });
-                if (updates?.length) {
-                    await socket.executeAsGM('updateActors', data, updates, healed);
-                }
-            }
-        }
-    });
+//             let { targets } = etc.templateData;
+//             targets = targets.filter(t => !myDisposition || targets.length === 1 || myDisposition === t.tokenData.disposition);
+//             const data = targets?.map((target) => {
+//                 const currentHp = target.actorData.data.attributes.hp.value;
+//                 const maxHp = target.actorData.data.attributes.hp.max;
+//                 const newHp = Math.min(currentHp + healed, maxHp);
+//                 return { id: target.actorData._id, currentHp, newHp };
+//             }) ?? [];
+//             const updates = data.map(({ id, newHp }) => ({ _id: id, newHp }));
+//             // });
+//             if (updates?.length) {
+//                 await socket.executeAsGM('updateActors', data, updates, healed);
+//             }
+//         }
+//     }
+// });
+
+Hooks.on('createChatMessage', async (doc, _options, userId) => {
+    if (!isFirstGM()) {
+        return;
+    }
+
+    // const targets = doc.targets; // bugged in pf1 9.4
+    const targetIds = doc.flags?.pf1?.metadata?.targets ?? [];
+    const targets = canvas.tokens.placeables.filter((o) => targetIds.includes(o.id));
+    const item = doc.itemSource;
+    const action = item?.actions.get(doc.flags?.pf1?.metadata?.action);
+    const attacks = doc.flags?.pf1?.metadata?.rolls?.attacks;
+    const isHealing = action?.isHealing;
+
+    ifDebug(() => {
+        console.log('Checking chat card for Heal Friendly Targets');
+        console.log(' - targets:', targets.map(x => x.id));
+        console.log(' - item:', item);
+        console.log(' - action:', action);
+        console.log(' - isHealing:', isHealing);
+    })
+
+    if (!targets?.length || !action || !attacks?.length || !isHealing) {
+        ifDebug(() => console.log(' - failed check, this is not healing, there was nothing rolled, or there were no targets'));
+        return;
+    }
+
+    const mod = isHealing ? -1 : 1;
+
+    const total = attacks
+        .flatMap((attack) => attack.damage)
+        .map((damage) => damage.total)
+        .reduce((acc, curr) => acc + curr, 0);
+
+    ifDebug(() => console.log(` - healint targets for [[${total}]]`));
+    await pf1.documents.actor.ActorPF.applyDamage(total * mod, { targets });
+
+    const content = game.i18n.format(
+        `${MODULE_NAME}.healCardDescription`,
+        { amountHealed: total, targets: targets.map(x => x.name).join(', ') },
+    );
+
+    const chatOptions = {
+        user: userId,
+        speaker: doc.speaker,
+        content,
+    };
+    ChatMessage.create(chatOptions);
 });
