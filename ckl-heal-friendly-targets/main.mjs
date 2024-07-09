08657983c8185146a4bccf43497747ce524c45e8
dmrickey <dmrickey@gmail.com>
2024-07-09 11:23:14 -0500

    Updated heal for v10

--- ckl-heal-friendly-targets/main.mjs
+++ ckl-heal-friendly-targets/main.mjs
@@ -1,35 +1,6 @@
-let socket;
-
 const MODULE_NAME = 'ckl-heal-friendly-targets';
 
-const getFirstActiveGM = () => game.users.activeGM;
-const isFirstGM = () => getFirstActiveGM() === game.user;
-
-// Hooks.once("socketlib.ready", () => {
-//     socket = socketlib.registerModule(MODULE_NAME);
-//     socket.register("updateActors", updateActors);
-// });
-
-// async function updateActors(data, updates, amountHealed) {
-//     if (updates?.length) {
-//         await Actor.updateDocuments(updates);
-
-//         const ids = updates.map(x => x._id);
-//         const actors = game.actors.filter(x => ids.includes(x.id));
-//         const targets = actors.map(x => x.name).join(', ');
-//         const content = game.i18n.format(
-//             `${MODULE_NAME}.healCardDescription`,
-//             { amountHealed, targets },
-//         );
-
-//         let chatOptions = {
-//             user: game.user._id,
-//             speaker: ChatMessage.getSpeaker(),
-//             content,
-//         };
-//         ChatMessage.create(chatOptions);
-//     }
-// }
+const isFirstGM = () => !!game.users.activeGM?.isSelf;
 
 const self = (me) => typeof me === 'function' ? me() : me;
 
@@ -40,45 +11,14 @@ const ifDebug = (func) => {
     }
 };
 
-// Hooks.on('itemUse', async (item, type, etc) => {
-//     if (type === 'attack') {
-//         ifDebug(() => console.log('************** itemUse hook ********************', item, type, etc));
-//     }
-//     if (type === 'postAttack') {
-//         ifDebug(() => console.log('******************* postAttack hook *********************', item, type, etc));
-//         if (item.isHealing) {
-//             const healed = etc.templateData.attacks[0]?.damage.total;
-//             if (!healed) {
-//                 return;
-//             }
-
-//             const myDisposition = item.actor?.getActiveTokens()?.[0]?.data.disposition ?? 0;
-
-//             let { targets } = etc.templateData;
-//             targets = targets.filter(t => !myDisposition || targets.length === 1 || myDisposition === t.tokenData.disposition);
-//             const data = targets?.map((target) => {
-//                 const currentHp = target.actorData.data.attributes.hp.value;
-//                 const maxHp = target.actorData.data.attributes.hp.max;
-//                 const newHp = Math.min(currentHp + healed, maxHp);
-//                 return { id: target.actorData._id, currentHp, newHp };
-//             }) ?? [];
-//             const updates = data.map(({ id, newHp }) => ({ _id: id, newHp }));
-//             // });
-//             if (updates?.length) {
-//                 await socket.executeAsGM('updateActors', data, updates, healed);
-//             }
-//         }
-//     }
-// });
-
 Hooks.on('createChatMessage', async (doc, _options, userId) => {
     if (!isFirstGM()) {
         return;
     }
 
-    // const targets = doc.targets; // bugged in pf1 9.4
+    // const targets = doc.targets; // bugged in pf1 9.4 and still bugged in 10.4
     const targetIds = doc.flags?.pf1?.metadata?.targets ?? [];
-    const targets = canvas.tokens.placeables.filter((o) => targetIds.includes(o.id));
+    const targets = targetIds.map((uuid) => fromUuidSync(uuid)?.object).filter(x => !!x);
     const item = doc.itemSource;
     const action = item?.actions.get(doc.flags?.pf1?.metadata?.action);
     const attacks = doc.flags?.pf1?.metadata?.rolls?.attacks;
@@ -104,7 +44,7 @@ Hooks.on('createChatMessage', async (doc, _options, userId) => {
         .map((damage) => damage.total)
         .reduce((acc, curr) => acc + curr, 0);
 
-    ifDebug(() => console.log(` - healint targets for [[${total}]]`));
+    ifDebug(() => console.log(` - healing targets for [[${total}]]`));
     await pf1.documents.actor.ActorPF.applyDamage(total * mod, { targets });
 
     const content = game.i18n.format(
